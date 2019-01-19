075c39e8d9dc4e9214778d5c89f4b726d266da9e
ducky <ivan@getin.co.il>
2019-01-19 16:43:40 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -109,7 +109,13 @@ app.post('/users/login', (req,res) => {
             res.header('x-auth', token).send(user);
         });
     }).catch((e) => res.status(400).send({}))
-})
+});
+
+app.delete('/users/me/token', authenticate,(req,res) => {
+    req.user.removeToken(req.token).then(
+    (success) => res.status(200).send(),
+    (error) => res.status(400).send())
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
